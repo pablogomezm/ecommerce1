6fb006494845adb08d223ed869cdde1772039c9c
pablogomezm <pablog.acc@gmail.com>
2026-02-14 12:24:49 -0300

    set android minSdk and targetSdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,10 +22,8 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.ecommerce1"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }

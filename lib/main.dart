83b20935f61b8fada40b866a0cd2c0c58f1bd722
pablogomezm <pablog.acc@gmail.com>
2026-02-22 17:21:46 -0300

    skip FCM token in ios simulator
    ios simulator can't get an FCM token, only real devices can get an FCM token

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'router.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +13,16 @@ Future<void> main() async {
 
   final messaging = FirebaseMessaging.instance;
   await messaging.requestPermission();
+
+  if (defaultTargetPlatform == TargetPlatform.iOS) {
+    final apnsToken = await messaging.getAPNSToken();
+    if (apnsToken == null) {
+      debugPrint('FCM Token: unavailable (iOS simulator has no APNS token)');
+      runApp(const MainApp());
+      return;
+    }
+  }
+
   final token = await messaging.getToken();
   debugPrint('FCM Token: $token');
 
